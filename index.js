81ba4b3cc23aa671018d6fc7c1bc684e9896bb6e
JazzPants <leighton.l14@gmail.com>
2021-10-28 10:38:17 +1100

    Second Commit

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ function introduction(name) {
     return (`Hi, my name is ${name}.`);
 }
 
-introduction("Josh");
-
 function introductionWithLanguage(name, language) {
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 
-introduction("Josh", "Ember.js");
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 
-introductionWithLanguageOptional("Gracie", "");
+//introductionWithLanguageOptional("Gracie"); second variable is omitted, so it goes to default
 
-introductionWithLanguageOptional("Gracie", "hello.js");
+//introductionWithLanguageOptional("Gracie", "hello.js"); second variable overrides default
 
