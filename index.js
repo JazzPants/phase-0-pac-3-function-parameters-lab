0d30a76993b5d2e36cde07c54c3ee72e7dea8a68
JazzPants <leighton.l14@gmail.com>
2021-10-28 10:30:51 +1100

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
+}
+
+introduction("Josh");
+
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+introduction("Josh", "Ember.js");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+introductionWithLanguageOptional("Gracie", "");
+
+introductionWithLanguageOptional("Gracie", "hello.js");
+
