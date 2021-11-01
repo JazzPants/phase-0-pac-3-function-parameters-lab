02f4406df56601742db3077236ef29c7b4b9f0dc
JazzPants <leighton.l14@gmail.com>
2021-11-02 01:41:09 +1100

    Second Commit

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function introductionWithLanguageOptional(name, language = "JavaScript") {
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 
+console.log(introductionWithLanguageOptional("Gracie")) //second argument defaults to JavaScript
+console.log(introductionWithLanguageOptional("Gracie", "hello.js"))
 //introductionWithLanguageOptional("Gracie"); second variable is omitted, so it goes to default
 
 //introductionWithLanguageOptional("Gracie", "hello.js"); second variable overrides default
